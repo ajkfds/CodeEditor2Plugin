a91a11bf9d787cde42c6c785c886a25afdaa8070
ajkfds <ajkfds2018@gmail.com>
2024-03-16 04:11:54 +0900

    initial commit

---
+++ CodeEditor2Plugin/CodeEditor2Plugin/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CodeEditor2Plugin.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ CodeEditor2Plugin/CodeEditor2Plugin/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace CodeEditor2Plugin.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
