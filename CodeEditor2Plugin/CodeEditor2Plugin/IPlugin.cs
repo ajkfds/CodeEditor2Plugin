48d98d0fec3e8e2472b4f21a5c2b9c27de5dd825
ajkfds <ajkfds2018@gmail.com>
2024-06-13 06:56:53 +0900

    add comments

--- CodeEditor2Plugin/CodeEditor2Plugin/IPlugin.cs
+++ CodeEditor2Plugin/CodeEditor2Plugin/IPlugin.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CodeEditor2Plugin
 {
+    /*
+    This interface is used to register/initialize a plugin for CodeEditor2.
+    Since individual features are implemented by adding callbacks to each object in CodeEditor from the plugin side,
+    we do not implement any functionality here other than registration/initialization.   
+     */
     public interface IPlugin
     {
+        // called first on bootup CodeEditor2. Register Plugin to CodeEditor2
+        // return true if you complete register this plugin
         bool Register();
+
+        // called after loading all Projects
         bool Initialize();
+
+        // plugin ID. must be unique in all used Plugins
         string Id { get; }
     }
 }
