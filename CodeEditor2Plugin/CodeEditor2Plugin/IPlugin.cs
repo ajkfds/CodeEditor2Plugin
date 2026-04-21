a2687e8c1e99cd3e21c87ec61d588e8dde06b48e
ajkfds <ajkfds2018@gmail.com>
2026-04-21 09:02:18 +0900

    code cleanup

--- CodeEditor2Plugin/CodeEditor2Plugin/IPlugin.cs
+++ CodeEditor2Plugin/CodeEditor2Plugin/IPlugin.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
 namespace CodeEditor2Plugin
 {
     /*
--- CodeEditor2Plugin/CodeEditor2Plugin/PulginSetup.cs
+++ CodeEditor2Plugin/CodeEditor2Plugin/PulginSetup.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CodeEditor2Plugin
 {
     public class PluginSetup
